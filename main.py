1b1c95f2f7a413e95e42dade3347446d152de6bb
smartinussen <steinar.martinussen@icloud.com>
2025-04-21 17:43:59 +0200

    Troublesome commit. Not solved without AI help :-( Really struggle with understanding dictionaries and how to use and access

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
 ============ BOOKBOT ============
 Analyzing book found at books/frankenstein.txt...
 ----------- Word Count ----------''')
-    print(f"{word_count} words found in the document")
+    print(f"Found {word_count} total words")
     # book_character_count(get_book_text("books/frankenstein.txt"))
     print("--------- Character Count -------")
 
