c2e1e498ec5d3edc4307cb7882768fe9f0da2f13
smartinussen <steinar.martinussen@icloud.com>
2025-04-21 18:05:00 +0200

    Updated project with arguments and more books. Can now take path and filename as input

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
 from stats import book_word_count, book_character_count, book_report
+
 
 def get_book_text(path):
     with open(path) as book:
@@ -6,12 +8,15 @@ def get_book_text(path):
     return text
 
 def main():
-    # get_book_text("books/frankenstein.txt")
-    word_count = book_word_count(get_book_text("books/frankenstein.txt"))
-    sorted_list = book_report(book_character_count(get_book_text("books/frankenstein.txt")))
-    print('''
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    word_count = book_word_count(get_book_text(sys.argv[1]))
+    sorted_list = book_report(book_character_count(get_book_text(sys.argv[1])))
+    print(f'''
 ============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------''')
     print(f"Found {word_count} total words")
     # book_character_count(get_book_text("books/frankenstein.txt"))
